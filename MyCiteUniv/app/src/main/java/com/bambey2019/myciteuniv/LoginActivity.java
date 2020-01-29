be2e842707a4d2e4d64c0d3787b124580cc88fc5
Fabita94 <adja.loucoubar@gmail.com>
2020-01-29 17:57:04 +0100

    version 2

--- MyCiteUniv/app/src/main/java/com/bambey2019/myciteuniv/LoginActivity.java
+++ MyCiteUniv/app/src/main/java/com/bambey2019/myciteuniv/LoginActivity.java
@@ -7,21 +7,26 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflat = getMenuInflater();
+        inflat.inflate(R.menu.custom_menu, menu);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btnConnexion = (Button) findViewById(R.id.coonexion);
-        btnConnexion.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_login);
+        Button btnReserv = (Button) findViewById(R.id.button);
+        btnReserv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final Intent intentAutre = new Intent(getApplicationContext(), LoginActivity.class);
+                final Intent intentAutre = new Intent(getApplicationContext(), PavillonActivity.class);
                 startActivity(intentAutre);
                 finish();
             }
